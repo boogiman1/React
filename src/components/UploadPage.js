d56ebb0db25148fe1ac15f72a6698625f5432ae3
boogiman1 <boowon1@naver.com>
2022-10-06 17:33:22 +0900

    서버 파일 처리

--- src/components/UploadPage.js
+++ src/components/UploadPage.js
@@ -1,20 +1,55 @@
-// import { useParams, useNavigate } from "react-router-dom";
-import { Form, Divider, InputNumber, Button, Upload, Input } from "antd";
+import { API_URL } from "../config/constants";
+import { useState } from "react";
+import { axios } from "axios";
+import { useNavigate } from "react-router-dom";
+import { Form, Divider, InputNumber, Button, Upload, Input, message } from "antd";
 import "./UploadPage.css";
 const UploadPage = () => {
+    const [imageUrl, setImageUrl] = useState(null)
+    const navi = useNavigate();
     const onFinish = (values) => {
-        console.log('Success:', values);
+        axios
+        .post(`${API_URL}/products`,{
+            name:values.name,
+            description:values.description,
+            seller:values.seller,
+            price:parseInt(values.price), //parseInt : 숫자를 정수로 바꿔주는 함수
+            imageUrl:imageUrl,
+        })
+        .then((result)=>{
+            navi('/',{replace:true})}
+        ).catch(()=>{
+            message.error('상품 등록에 오류가 있습니다.');
+        })
     };
+    const onChangeImage = (info) => {
+        if (info.file.status === "uploading") {
+            return;
+        }
+        if (info.file.status === "done") {
+            const response = info.file.response;
+            const imageUrl = response.imageUrl;
+            setImageUrl(imageUrl);
+        }
+        //이미지가 바뀔때 실행하는 함수
+    }
     //   onFinish 는 form이 제출이 되면 값을 가질 함수
     return (
         <div id="body">
             <div id="upload-container">
                 <Form name="uploadForm" onFinish={onFinish}>
                     <Form.Item name="upload">
-                        <div id="upload-img">
-                            <img src="/images/icons/camera.png" />
-                            <span>상품 이미지를 업로드 해주세요</span>
-                        </div>
+                        <Upload name="image" action={`${API_URL}/image`} listType="picture" showUploadList={false} onChange={onChangeImage} >
+                            {imageUrl ? (
+                                <img id="upload-img" src={`${API_URL}/${imageUrl}`} />
+                            ) : (
+                                <div id="upload-img-placeholder">
+                                    <img src="/images/icons/camera.png" />
+                                    <span>상품 이미지를 업로드 해주세요</span>
+                                </div>
+
+                            )}
+                        </Upload>
                     </Form.Item>
                     {/* Divider : hr 같은 역할 form 태그 사이의 구분이 필요하다 */}
                     <Divider />
---
+++ src/config/constants.js
@@ -0,0 +1,2 @@
+//constants.js
+export const API_URL="http://localhost:8080";
